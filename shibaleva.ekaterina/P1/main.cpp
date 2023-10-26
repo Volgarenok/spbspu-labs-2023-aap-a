cfc1c3e1378be73342343563808e5b62db775fe8
eshikta <katya.shibaleva@gmail.com>
2023-10-26 04:18:02 +0300

    Count limit

--- shibaleva.ekaterina/P1/main.cpp
+++ shibaleva.ekaterina/P1/main.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+#include <limits>
+ 
 int main()
-{}
+{
+  int number = 0;
+  size_t count = 0;
+  do
+  {
+    std::cin >> number;
+    if (!std::cin)
+    {
+      std::cerr << "Is not a sequence\n";
+      return 1;
+    }
+    else if (number != 0)
+    {
+      size_t max_size = std::numeric_limits< size_t >::max();
+      if (count == max_size)
+      {
+        std::cerr << "Sequence is too long\n";
+        return 2;
+      }
+      ++count;
+    }
+  }
+  while (number != 0);
+  std::cout << count << "\n";
+}
