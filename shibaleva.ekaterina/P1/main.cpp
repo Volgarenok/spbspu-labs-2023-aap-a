2d419c297f96b67658aa5bf5179c58b10f1c77ac
eshikta <katya.shibaleva@gmail.com>
2023-10-30 16:10:41 +0300

    small sequence, count2, answer

--- shibaleva.ekaterina/P1/main.cpp
+++ shibaleva.ekaterina/P1/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <limits>
- 
 int main()
 {
   int number = 0;
   size_t count = 0;
+  int last = 0;
+  size_t count2 = 0;
+  int answer = 0;
   do
   {
     std::cin >> number;
@@ -18,12 +20,31 @@ int main()
       size_t max_size = std::numeric_limits< size_t >::max();
       if (count == max_size)
       {
-        std::cerr << "Sequence is too long\n";
+        std::cerr << "Sequence is too long!\n";
         return 2;
       }
       ++count;
+      if (count == 1)
+      {
+        last = number;
+      }
+      if (count >= 2 and last > number and number != 0)
+      {
+        ++count2;
+        if (count2 == 2)
+        {
+          count2 = 0;
+          ++answer;
+        }
+      }
     }
   }
   while (number != 0);
-  std::cout << count << "\n";
+  if (number == 0 and count < 3)
+  {
+    std::cerr << "Sequence is too small!\n";
+    return 2;
+  }
+  std::cout << "Длина последовательности: " << count << "\n";
+  std::cout << "Ответ: " << answer << "\n";
 }
