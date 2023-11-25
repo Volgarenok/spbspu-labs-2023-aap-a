a580ec65eac674f5bc52ec6ff772215f5a0d3d18
Tugce serter <147084872+mafeeoet@users.noreply.github.com>
2023-11-25 23:37:13 +0300

    Update main.cpp

--- serter.tugce/P0/main.cpp
+++ serter.tugce/P0/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "credentials.hpp"
 
 int main()
@@ -7,10 +6,4 @@ int main()
    using namespace serter; 
    outCredentials();
    std::cout << "\n";
-=======
-
-int main()
-{
-  std::cout << "serter.tugce\n";
->>>>>>> 4133097689ca6422f3be2430e585e4a530d6741d
 }
