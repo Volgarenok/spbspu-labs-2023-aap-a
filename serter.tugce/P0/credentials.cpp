a38303e56c7bfb88a16c57821dbf2ee9fd32f524
mafeeoet <serter.t@spbstu.ru>
2023-11-28 21:03:19 +0300

    Revert "TU for P0"
    This reverts commit 3fd056574b4cb1d2ae811808513eb89ecddaca08.

--- serter.tugce/P0/credentials.cpp
+++ serter.tugce/P0/credentials.cpp
@@ -1,9 +1,6 @@
 #include "credentials.hpp"
 #include <iostream>
-<<<<<<< HEAD
 void serter::outCredentials(const Credentials & cr)
 {
-    std::cout << cr.second << "." << cr.first
-    void serter::outCredentials()
-{
-
+    std::cout << cr.second << "." << cr.first;
+}
