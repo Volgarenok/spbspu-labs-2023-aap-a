a58714434302f6521646c2b7e9de2c83107bc523
mafeeoet <serter.t@spbstu.ru>
2023-11-28 21:03:35 +0300

    P0 first tryy

--- serter.tugce/P0/credentials.cpp
+++ serter.tugce/P0/credentials.cpp
@@ -1,6 +1,9 @@
 #include "credentials.hpp"
 #include <iostream>
+<<<<<<< HEAD
 void serter::outCredentials(const Credentials & cr)
 {
-    std::cout << cr.second << "." << cr.first;
-}
+    std::cout << cr.second << "." << cr.first
+    void serter::outCredentials()
+{
+
