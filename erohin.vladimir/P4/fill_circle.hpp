4e71f27318753b016ab4877d16c4feb4908a002b
mafeeoet <serter.t@spbstu.ru>
2023-12-21 19:24:53 +0300

    Revert "Merge pull request #92 from WarriorVortex/erohin.vladimir/P4"
    This reverts commit 92ec1c293e43c49df0fbd442a25c596bd5c6da89, reversing
    changes made to 0d0880d0dc8ef25725d010f8b4e5c7f2388c6df4.

--- erohin.vladimir/P4/fill_circle.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FILL_CIRCLE_HPP
-#define FILL_CIRCLE_HPP
-
-#include <cstddef>
-namespace erohin
-{
-  void fillCircle(int * matrix, size_t rows, size_t cols);
-}
-#endif
--- erohin.vladimir/P4/max_main_diagonal.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MAX_MAIN_DIAGONAL_HPP
-#define MAX_MAIN_DIAGONAL_HPP
-
-#include <cstddef>
-namespace erohin
-{
-  long long findMaxMainDiagonal(const int * matrix, size_t rows, size_t cols);
-}
-#endif
