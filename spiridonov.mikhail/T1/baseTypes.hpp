3684c05dabc073939eb58a0dbd4b23f5e020b66b
mordw1n <lolpovar0@gmail.com>
2023-12-25 06:17:05 +0300

    base types add

---
+++ spiridonov.mikhail/T1/baseTypes.hpp
@@ -0,0 +1,20 @@
+#ifndef BASE_TYPES_HPP
+#define	BASE_TYPES_HPP
+
+namespace spiridonov
+{
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+
+  struct rectangle_t
+  {
+    double width;
+    double height;
+    double pos;
+  };
+}
+
+#endif
