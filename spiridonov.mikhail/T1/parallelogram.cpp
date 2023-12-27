b33a49dc9e68f1cf4e0e89993167b54ce8abc57a
mordw1n <lolpovar0@gmail.com>
2023-12-27 16:20:13 +0300

    initial for functions parallelogram.cpp

---
+++ spiridonov.mikhail/T1/parallelogram.cpp
@@ -0,0 +1,34 @@
+#include "parallelogram.hpp"
+#include <stdexcept>
+#include <cmath>
+
+namespace spiridonov
+{
+  Parallelogram::Parallelogram(rectangle_t rect) :
+  {}
+
+  double Parallelogram::getArea()
+  {
+
+  }
+
+  rectangle_t Parallelogram::getFrameRect()
+  {
+
+  }
+
+  void Parallelogram::move(point_t pos)
+  {
+
+  }
+
+  void Parallelogram::move(double x, double y)
+  {
+
+  }
+
+  void Parallelogram::scale(double coefficient)
+  {
+
+  }
+}
