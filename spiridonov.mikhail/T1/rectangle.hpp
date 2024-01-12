bd6a62ea00fc87a7b33245d75d26e19c31b07313
mordw1n <lolpovar0@gmail.com>
2024-01-12 05:11:45 +0300

    fix rectangle hpp

--- spiridonov.mikhail/T1/rectangle.hpp
+++ spiridonov.mikhail/T1/rectangle.hpp
@@ -3,27 +3,23 @@
 
 namespace spiridonov
 {
-  class Rectangle : public Shape
-  {
-    public:
-      Rectangle() : frameRect_{ 0.0, 0.0, {0.0, 0.0} }
-      {}
-      Rectangle(double width, double height, double x, double y) :
-        frameRect_{ width, height, {x, y} }
-      {}
-      double getArea();
-      rectangle_t getFrameRect();
-      void move(point_t pos) override;
-      void move(double x, double y) override;
-      void scale(double coefficient) override;
-      Shape* clone() const override
-      {
-        return new Rectangle(*this);
-      }
-      ~Rectangle() = default;
-      private:
-        rectangle_t frameRect_;
-    };
+	class Rectangle : public Shape
+	{
+	public:
+		Rectangle() : frameRect_{ 0.0, 0.0, {0.0, 0.0} }
+		{}
+		Rectangle(double width, double height, double x, double y) :
+			frameRect_{ width, height, {x, y} }
+		{}
+		double getArea() override;
+		rectangle_t getFrameRect() override;
+		void move(point_t pos) override;
+		void move(double x, double y) override;
+		void scale(double coefficient) override;
+		~Rectangle() = default;
+	private:
+		rectangle_t frameRect_;
+	};
 }
 
 #endif
