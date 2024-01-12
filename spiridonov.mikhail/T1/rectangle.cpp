2d98300038f7d22363e895074ec6a26be11f0d8a
mordw1n <lolpovar0@gmail.com>
2024-01-12 05:08:28 +0300

    ahahha neudachno perenes s vizualki, fix CG

--- spiridonov.mikhail/T1/rectangle.cpp
+++ spiridonov.mikhail/T1/rectangle.cpp
@@ -3,34 +3,34 @@
 
 namespace spiridonov
 {
-	double Rectangle::getArea()
-	{
-		return frameRect_.height * frameRect_.width;
-	}
+  double Rectangle::getArea()
+  {
+    return frameRect_.height * frameRect_.width;
+  }
 
-	rectangle_t Rectangle::getFrameRect()
-	{
-		return frameRect_;
-	}
+  rectangle_t Rectangle::getFrameRect()
+  {
+    return frameRect_;
+  }
 
-	void Rectangle::move(point_t pos)
-	{
-		frameRect_.pos = pos;
-	}
+  void Rectangle::move(point_t pos)
+  {
+    frameRect_.pos = pos;
+  }
 
-	void Rectangle::move(double x, double y)
-	{
-		frameRect_.pos.x += x;
-		frameRect_.pos.y += y;
-	}
+  void Rectangle::move(double x, double y)
+  {
+    frameRect_.pos.x += x;
+    frameRect_.pos.y += y;
+  }
 
-	void Rectangle::scale(double coefficient)
-	{
-		if (coefficient <= 0)
-		{
-			throw std::invalid_argument("Error: invalid coefficient to scale");
-		}
-		frameRect_.height *= coefficient;
-		frameRect_.width *= coefficient;
-	}
+  void Rectangle::scale(double coefficient)
+  {
+    if (coefficient <= 0)
+    {
+      throw std::invalid_argument("Error: invalid coefficient to scale");
+    }
+    frameRect_.height *= coefficient;
+    frameRect_.width *= coefficient;
+  }
 }
