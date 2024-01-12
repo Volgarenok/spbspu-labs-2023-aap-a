954e5f07fb96028dc8cfb51817ddda269f8d8a47
mordw1n <lolpovar0@gmail.com>
2024-01-12 04:49:27 +0300

    fix compShape hpp

--- spiridonov.mikhail/T1/compositeShape.hpp
+++ spiridonov.mikhail/T1/compositeShape.hpp
@@ -4,27 +4,28 @@
 
 namespace spiridonov
 {
-  struct CompositeShape
+  class CompositeShape : public Shape
   {
-    size_t shapes;
-    size_t capacity;
-    Shape* shapeptrs;
-
+  public:
     CompositeShape();
-    CompositeShape(const CompositeShape& other);
-    CompositeShape(CompositeShape&& other) noexcept;
     ~CompositeShape();
 
-    CompositeShape& operator=(const CompositeShape& other);
-    CompositeShape& operator=(CompositeShape&& other) noexcept;
-
-    void addShape(const Shape& shape);
+    void addShape(Shape* shape);
     void removeShape(size_t index);
-    double getArea() const;
-    rectangle_t getFrameRect() const;
-    void move(point_t pos);
-    void move(double x, double y);
-    void scale(double coefficient);
+    double getArea() override;
+    rectangle_t getFrameRect() override;
+    void move(point_t pos) override;
+    void move(double x, double y) override;
+    void scale(double coefficient) override;
+    Shape* getShape(size_t index) const;
+    void clear();
+
+
+    size_t getShapesCount() const;
+
+  private:
+    Shape** shapePtrs;
+    size_t shapes;
   };
 }
 
