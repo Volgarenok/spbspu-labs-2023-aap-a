8d81cb71a78097989d7c9f44008afbb04754bde0
mordw1n <lolpovar0@gmail.com>
2024-01-12 05:15:42 +0300

    snova CG

--- spiridonov.mikhail/T1/rectangle.hpp
+++ spiridonov.mikhail/T1/rectangle.hpp
@@ -3,23 +3,23 @@
 
 namespace spiridonov
 {
-	class Rectangle : public Shape
-	{
-	public:
-		Rectangle() : frameRect_{ 0.0, 0.0, {0.0, 0.0} }
-		{}
-		Rectangle(double width, double height, double x, double y) :
-			frameRect_{ width, height, {x, y} }
-		{}
-		double getArea() override;
-		rectangle_t getFrameRect() override;
-		void move(point_t pos) override;
-		void move(double x, double y) override;
-		void scale(double coefficient) override;
-		~Rectangle() = default;
-	private:
-		rectangle_t frameRect_;
-	};
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle() : frameRect_{ 0.0, 0.0, {0.0, 0.0} }
+    {}
+    Rectangle(double width, double height, double x, double y) :
+      frameRect_{ width, height, {x, y} }
+    {}
+    double getArea() override;
+    rectangle_t getFrameRect() override;
+    void move(point_t pos) override;
+    void move(double x, double y) override;
+    void scale(double coefficient) override;
+    ~Rectangle() = default;
+  private:
+    rectangle_t frameRect_;
+  };
 }
 
 #endif
