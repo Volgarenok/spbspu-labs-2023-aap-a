1a61b91e2a075cdfde799a89c0541d14e341f3e4
mordw1n <lolpovar0@gmail.com>
2024-01-12 05:00:30 +0300

    fix parallelogram hpp

--- spiridonov.mikhail/T1/parallelogram.hpp
+++ spiridonov.mikhail/T1/parallelogram.hpp
@@ -10,13 +10,16 @@ namespace spiridonov
   public:
     Parallelogram() : arrayOfPoints{ {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0} }
     {}
+    Parallelogram(double x1, double x2, double x3, double y1, double y2, double y3) :
+      arrayOfPoints{ {x1, y1}, {x2, y2}, {x3, y3} }
+    {}
     Parallelogram(rectangle_t rect);
     double getArea();
     rectangle_t getFrameRect();
     void move(point_t pos) override;
     void move(double x, double y) override;
     void scale(double coefficient) override;
-    ~Parallelogram();
+    ~Parallelogram() = default;
   private:
     point_t arrayOfPoints[3];
   };
