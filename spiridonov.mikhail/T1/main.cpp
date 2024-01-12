a161a82c2ff4d4724b9f813a3607745765065df7
mordw1n <lolpovar0@gmail.com>
2024-01-12 05:40:38 +0300

    add conditions in main

--- spiridonov.mikhail/T1/main.cpp
+++ spiridonov.mikhail/T1/main.cpp
@@ -64,6 +64,33 @@ int main()
       composite.move(dx, dy);
     }
 
+    else if (input == "CONCAVE")
+    {
+      double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0, x4 = 0.0, y4 = 0.0;
+      std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+
+      if (std::cin.fail())
+      {
+        std::cerr << "Error: Invalid concave parameters\n";
+        return 1;
+      }
+
+      composite.addShape(new Concave({ x1, y1 }, { x2, y2 }, { x3, y3 }, { x4, y4 }));
+    }
+
+    else if (input == "PARALLELOGRAM")
+    {
+      double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0;
+      std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+      if (std::cin.fail())
+      {
+        std::cerr << "Error: Invalid parallelogram parameters\n";
+        return 1;
+      }
+
+      composite.addShape(new Parallelogram(x1, x2, x3, y1, y2, y3));
+
     else if (input == "END")
     {
       break;
