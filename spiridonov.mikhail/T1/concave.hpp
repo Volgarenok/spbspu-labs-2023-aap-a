58d0ccd69c149f2f10509089b47990e7dc1ebe80
mordw1n <lolpovar0@gmail.com>
2024-01-12 04:56:49 +0300

    fix concave hpp

--- spiridonov.mikhail/T1/concave.hpp
+++ spiridonov.mikhail/T1/concave.hpp
@@ -8,17 +8,20 @@ namespace spiridonov
   class Concave : public Shape
   {
   public:
-    Concave() : arrayOfPoints{ {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0} }
+    Concave() : arrayOfPoints{ {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0} },
+      frameRect_{ 0.0, 0.0, {0.0, 0.0} }
     {}
     Concave(point_t p1, point_t p2, point_t p3, point_t p4);
-    double getArea();
-    rectangle_t getFrameRect();
+    double getArea() override;
+    rectangle_t getFrameRect() override;
     void move(point_t pos) override;
     void move(double x, double y) override;
     void scale(double coefficient) override;
-    ~Concave();
+    ~Concave() = default;
   private:
     point_t arrayOfPoints[4];
+    rectangle_t frameRect_;
+    void updateFrameRect();
   };
 }
 
