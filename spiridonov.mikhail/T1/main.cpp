ae49fcc8dd7f5d463a1d6aed35795e0c4b921a50
mordw1n <lolpovar0@gmail.com>
2024-01-12 04:36:33 +0300

    main

--- spiridonov.mikhail/T1/main.cpp
+++ spiridonov.mikhail/T1/main.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
+#include <string>
+#include "baseTypes.hp"
+#include "rectangle.hpp"
+#include "shape.hpp"
+#include "compositeShape.hpp"
+#include "concave.hpp"
+#include "parallelogram.hpp"
 
 int main()
-{}
+{
+  using namespace spiridonov;
+
+  CompositeShape composite;
+
+  std::string input;
+  while (std::cin >> input)
+  {
+    if (input == "SCALE")
+    {
+      double centerX = 0.0, centerY = 0.0, scaleCoefficient = 0.0;
+      std::cin >> centerX >> centerY >> scaleCoefficient;
+
+      if (std::cin.fail())
+      {
+        std::cerr << "Error: Invalid scale parameters\n";
+        return 1;
+      }
+
+      std::cout << "Before scaling:\n";
+      for (size_t i = 0; i < composite.getShapesCount(); ++i)
+      {
+        Shape* shape = composite.getShape(i);
+        std::cout << shape->getArea() << " " << shape->getFrameRect().pos.x - shape->getFrameRect().width / 2
+          << " " << shape->getFrameRect().pos.y - shape->getFrameRect().height / 2
+          << " " << shape->getFrameRect().pos.x + shape->getFrameRect().width / 2
+          << " " << shape->getFrameRect().pos.y + shape->getFrameRect().height / 2 << "\n";
+      }
+
+      composite.scale(scaleCoefficient);
+
+      std::cout << "After scaling:\n";
+      for (size_t i = 0; i < composite.getShapesCount(); ++i)
+      {
+        Shape* shape = composite.getShape(i);
+        std::cout << shape->getArea() << " " << shape->getFrameRect().pos.x - shape->getFrameRect().width / 2
+          << " " << shape->getFrameRect().pos.y - shape->getFrameRect().height / 2
+          << " " << shape->getFrameRect().pos.x + shape->getFrameRect().width / 2
+          << " " << shape->getFrameRect().pos.y + shape->getFrameRect().height / 2 << "\n";
+      }
+    }
+    
+  return 0;
+}
 
