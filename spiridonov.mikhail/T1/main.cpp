2935e12424d115937eda90677534304d12afd53f
mordw1n <lolpovar0@gmail.com>
2024-02-16 01:40:05 +0300

    Fix main

--- spiridonov.mikhail/T1/main.cpp
+++ spiridonov.mikhail/T1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <string>
 #include <stdexcept>
 #include <iomanip>
@@ -15,124 +14,83 @@ int main()
   using namespace spiridonov;
   CompositeShape composite;
   std::string input = "";
-  bool scaleCommandFound = false;
-  bool shapesAdded = false;
-  bool invalidShapeDetected = false;
-  while (std::cin >> input)
+  bool badShape = false;
+  while(std::cin >> input)
   {
-    if (!input.empty())
+    try
     {
-      try
+      if (input == "RECTANGLE")
       {
-        if (input == "SCALE")
+        point_t p1, p2;
+        std::cin >> p1.x >> p1.y >> p2.x >> p2.y;
+        if(!std::cin || p1.x >= p2.x || p1.y >= p2.y)
         {
-          scaleCommandFound = true;
-          double centerX = 0.0, centerY = 0.0, scaleCoefficient = 0.0;
-          std::cin >> centerX >> centerY >> scaleCoefficient;
-          if (std::cin.fail() || scaleCoefficient <= 0)
-          {
-            std::cerr << "Error: Invalid scale command or scale coefficient\n";
-            return 1;
-          }
-          if (composite.getShapesCount() == 0)
-          {
-            std::cerr << "Error: Nothing to scale\n";
-            return 1;
-          }
-          std::cout << std::fixed << std::setprecision(1);
-          for (size_t i = 0; i < composite.getShapesCount(); ++i)
-          {
-            Shape* shape = composite.getShape(i);
-            rectangle_t frameRect = shape->getFrameRect();
-            std::cout << shape->getArea() << " "
-              << frameRect.pos.x - frameRect.width / 2 << " "
-              << frameRect.pos.y - frameRect.height / 2 << " "
-              << frameRect.pos.x + frameRect.width / 2 << " "
-              << frameRect.pos.y + frameRect.height / 2 << "\n";
-          }
-          composite.scale(scaleCoefficient);
-          for (size_t i = 0; i < composite.getShapesCount(); ++i)
-          {
-            Shape* shape = composite.getShape(i);
-            rectangle_t frameRect = shape->getFrameRect();
-            std::cout << shape->getArea() << " ";
-            std::cout << std::setprecision(1) << " " << std::round((frameRect.pos.x - (frameRect.width / 2)) * 10) / 10;
-            std::cout << std::setprecision(1) << " " << std::round((frameRect.pos.y - (frameRect.height / 2)) * 10) / 10;
-            std::cout << std::setprecision(1) << " " << std::round((frameRect.pos.x + (frameRect.width / 2)) * 10) / 10;
-            std::cout << std::setprecision(1) << " " << std::round((frameRect.pos.y + (frameRect.height / 2)) * 10) / 10 << "\n";
-          }
+          badShape = true;
         }
-        else if (input == "MOVE")
+        composite.addShape(new Rectangle({p1.x, p1.y}, {p2.x, p2.y}));
+      }
+      else if (input == "CONCAVE")
+      {
+        double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0, x4 = 0.0, y4 = 0.0;
+        std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+        if (!std::cin)
         {
-          double dx = 0.0, dy = 0.0;
-          std::cin >> dx >> dy;
-          if (std::cin.fail())
-          {
-            std::cerr << "Error: Invalid move parameters\n";
-            return 1;
-          }
-          composite.move(dx, dy);
+          badShape = true;
         }
-        else if (input == "RECTANGLE")
+        composite.addShape(new Concave({ x1, y1 }, { x2, y2 }, { x3, y3 }, { x4, y4 }));
+      }
+      else if (input == "PARALLELOGRAM")
+      {
+        double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0;
+        std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        if (!std::cin)
         {
-          shapesAdded = true;
-          point_t p1 = {0, 0};
-          point_t p2 = {0, 0};
-          std::cin >> p1.x >> p1.y >> p2.x >> p2.y;
-          if (std::cin.fail() || p1.x >= p2.x || p2.y >= p2.y)
-          {
-            invalidShapeDetected = true;
-            shapesAdded = false;
-          }
-          composite.addShape(new Rectangle(p1, p2));
+          badShape = true;
         }
-        else if (input == "CONCAVE")
+        composite.addShape(new Parallelogram(x1, x2, x3, y1, y2, y3));
+      }
+      else if(input == "SCALE")
+      {
+        double centerX = 0.0, centerY = 0.0, scaleCoefficient = 0.0;
+        std::cin >> centerX >> centerY >> scaleCoefficient;
+        if (std::cin && scaleCoefficient <= 0)
         {
-          shapesAdded = true;
-          double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0, x4 = 0.0, y4 = 0.0;
-          std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-          if (std::cin.fail())
-          {
-            std::cerr << "Error: Invalid concave parameters\n";
-            shapesAdded = false;
-            return 1;
-          }
-          composite.addShape(new Concave({ x1, y1 }, { x2, y2 }, { x3, y3 }, { x4, y4 }));
+          std::cerr << "Error: Invalid scale command or scale coefficient\n";
+          return 1;
         }
-        else if (input == "PARALLELOGRAM")
+        std::cout << std::fixed << std::setprecision(1);
+        for (size_t i = 0; i < composite.getShapesCount(); ++i)
         {
-          shapesAdded = true;
-          double x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0, x3 = 0.0, y3 = 0.0;
-          std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-          if (std::cin.fail())
-          {
-            std::cerr << "Error: Invalid parallelogram parameters\n";
-            shapesAdded = false;
-            return 1;
-          }
-          composite.addShape(new Parallelogram(x1, x2, x3, y1, y2, y3));
+          Shape* shape = composite.getShape(i);
+          rectangle_t frameRect = shape->getFrameRect();
+          std::cout << shape->getArea() << " "
+          << frameRect.pos.x - frameRect.width / 2 << " "
+          << frameRect.pos.y - frameRect.height / 2 << " "
+          << frameRect.pos.x + frameRect.width / 2 << " "
+          << frameRect.pos.y + frameRect.height / 2 << "\n";
         }
-        else if (input == "END")
+        composite.scale(scaleCoefficient);
+        for (size_t i = 0; i < composite.getShapesCount(); ++i)
         {
-          break;
+          Shape* shape = composite.getShape(i);
+          rectangle_t frameRect = shape->getFrameRect();
+          std::cout << shape->getArea() << " "
+          << frameRect.pos.x - frameRect.width / 2 << " "
+          << frameRect.pos.y - frameRect.height / 2 << " "
+          << frameRect.pos.x + frameRect.width / 2 << " "
+          << frameRect.pos.y + frameRect.height / 2 << "\n";
         }
       }
-      catch (const std::invalid_argument& e)
+      else if (badShape)
       {
-        std::cerr << "Error: " << e.what() << "\n";
-        return 1;
+        std::cerr << "Bad shape!" << '\n';
+        return 0;
       }
     }
+    catch (const std::invalid_argument& e)
+    {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
   }
-  if (invalidShapeDetected)
-  {
-    std::cout << "Invalid shape detected\n";
-    return 1;
-  }
-  if (!scaleCommandFound && shapesAdded)
-  {
-    std::cerr << "Error: Scaling command not found\n";
-    return 1;
-  }
-  return 0;
 }
--- spiridonov.mikhail/T1/rectangle.cpp
+++ spiridonov.mikhail/T1/rectangle.cpp
@@ -4,54 +4,54 @@
 
 namespace spiridonov
 {
-  Rectangle::Rectangle(const point_t& p1, const point_t& p2) :
-    bottomLeft_(p1),
-    topRight_(p2)
-  {}
+Rectangle::Rectangle(const point_t & p1, const point_t & p2):
+bottomLeft_(p1),
+topRight_(p2)
+{}
 
-  double Rectangle::getArea() const
-  {
+double Rectangle::getArea() const
+{
     return (topRight_.x - bottomLeft_.x) * (topRight_.y - bottomLeft_.y);
-  }
+}
 
-  rectangle_t Rectangle::getFrameRect() const
-  {
+rectangle_t Rectangle::getFrameRect() const
+{
     double width = topRight_.x - bottomLeft_.x;
     double height = topRight_.y - bottomLeft_.y;
-    point_t pos = { (topRight_.x + bottomLeft_.x) / 2, (topRight_.y + bottomLeft_.y) / 2 };
-    return { width, height, pos };
-  }
+    point_t pos = {(topRight_.x + bottomLeft_.x) / 2, (topRight_.y + bottomLeft_.y) / 2};
+    return {width, height, pos};
+}
 
-  void Rectangle::move(point_t pos)
-  {
+void Rectangle::move(point_t pos)
+{
     double width = topRight_.x - bottomLeft_.x;
     double height = topRight_.y - bottomLeft_.y;
-    topRight_ = { pos.x + (width / 2), pos.y + (height / 2) };
-    bottomLeft_ = { pos.x - (width / 2), pos.y - (height / 2) };
-  }
+    topRight_ = {pos.x + (width / 2), pos.y + (height / 2)};
+    bottomLeft_ = {pos.x - (width / 2), pos.y - (height / 2)};
+}
 
-  void Rectangle::move(double x, double y)
-  {
+void Rectangle::move(double x, double y)
+{
     topRight_.x += x;
     bottomLeft_.x += x;
     topRight_.y += y;
     bottomLeft_.y += y;
-  }
+}
 
-  void Rectangle::scale(double coefficient)
-  {
-    if (coefficient <= 0.0)
+void Rectangle::scale(double coefficient)
+{
+    if(coefficient <= 0.0)
     {
-      throw std::invalid_argument("Invalid coefficient");
+        throw std::invalid_argument("Invalid coefficient");
     }
     else
     {
-      double width = (topRight_.x - bottomLeft_.x) / 2;
-      double height = (topRight_.y - bottomLeft_.y) / 2;
-      topRight_.x += (coefficient - 1.0) * width;
-      topRight_.y += (coefficient - 1.0) * height;
-      bottomLeft_.x -= (coefficient - 1.0) * width;
-      bottomLeft_.y -= (coefficient - 1.0) * height;
+        double width = (topRight_.x - bottomLeft_.x) / 2;
+        double height = (topRight_.y - bottomLeft_.y)  / 2;
+        topRight_.x += (coefficient - 1.0) * width;
+        topRight_.y += (coefficient - 1.0) * height;
+        bottomLeft_.x -= (coefficient - 1.0) * width;
+        bottomLeft_.y -= (coefficient - 1.0) * height;
     }
-  }
+}
 }
