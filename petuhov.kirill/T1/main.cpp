38e7ba201330394bfb2aeac3966bafe0e97cc3bd
deadshumz <shumzz@bk.ru>
2024-03-11 16:24:52 +0300

    reading rectangle

--- petuhov.kirill/T1/main.cpp
+++ petuhov.kirill/T1/main.cpp
@@ -1,4 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "circle.hpp"
+
 int main()
 {
-  return;
+  petuhov::Shape * shapes[1000] = {};
+  size_t shapeCount = 0;
+  std::string command;
+
+  while (std::cin >> command && command != "SCALE")
+  {
+    if (command == "RECTANGLE")
+    {
+      float x1, y1, x2, y2 = 0;
+      std::cin >> x1 >> y1 >> x2 >> y2;
+      float centerX = (x1 + x2) / 2;
+      float centerY = (y1 + y2) / 2;
+      float width = std::abs(x1 - x2);
+      float height = std::abs(y1 - y2);
+      shapes[shapeCount++] = new petuhov::Rectangle({centerX, centerY}, width, height);
+    }
+  }
 }
