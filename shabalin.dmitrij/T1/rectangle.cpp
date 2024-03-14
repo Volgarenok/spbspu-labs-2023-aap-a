61533bcdf34874d4a4c2e5187b5497a356cb068f
dmitei <dima9990860349@icloud.com>
2024-03-14 14:39:51 +0300

    changes in rectangle files

---
+++ shabalin.dmitrij/T1/rectangle.cpp
@@ -0,0 +1,68 @@
+#include "base_type.hpp"
+#include <stdexcept>
+#include "rectangle.hpp"
+
+shabalin::Rectangle::Rectangle(point_t leftDownCorner, point_t rightUpperCorner)
+  pointLeft(p1),
+  pointRight(p2)
+{
+  if (pointLeft.y >= pointRight.y || pointLeft.x >= pointRight.x)
+  {
+    throw std::invalid_argument("invalid arguments")
+  }
+}
+
+double shabalin::Rectangle::getArea() const
+{
+  double height = std::abs(pointLeft.y - pointRight.y);
+  double width = std::abs(pointLeft.x - pointRight.x);
+  return height * width;
+}
+
+shabalin::rectangle_t shabalin::Rectangle::getFrameRect() const
+{
+  point_t center({(pointLeft.x + pointRight.x) / 2, (pointLeft.y + pointRight.y) / 2});
+  double height = std::abs(pointLeft.y - pointRight.y);
+  double width = std::abs(pointLeft.x - pointRight.x);
+  return {width, height, center};
+}
+
+void shabalin::Rectangle::move(const point_t newCenter)
+{
+  point_t center({(pointLeft.x + pointRight.x) / 2, (pointLeft.y + pointRight.y) / 2});
+  double del_x = newCenter.x - center.x;
+  double del_y = newCenter.y - center.y;
+  move(del_x, del_y);
+}
+
+void shabalin::Rectangle::move(double del_x, double del_y)
+{
+  point_t center({(pointLeft.x + pointRight.x) / 2, (pointLeft.y + pointRight.y)});
+  center.x = center.x + del_x;
+  center.y = center.y + del_y;
+  point_t *points[2] = {&pointLeft, &pointRight};
+  for (size_t i = 0; i < 2; ++i)
+  {
+    points[i]->x += del_x;
+    points[i]-> += del_y;
+  }
+}
+
+void shabalin::Rectangle::scaleShape(const double k)
+{
+  double width = std::abs(pointLeft.x - pointRight.x);
+  double height = std::abs(pointLeft.y - pointRight.y);
+
+  double newWidth = width * k;
+  double newHeight = height * k;
+
+  double del_x = (newWidth - width) / 2.0;
+  double del_y = (newHeight - height) / 2.0;
+
+  pointLeft.x -= del_x;
+  pointRight.x -= del_y;
+  pointLeft.y += del_x;
+  pointRight.y += del_y;
+}
+
+
---
+++ shabalin.dmitrij/T1/rectangle.hpp
@@ -0,0 +1,21 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "shape.hpp"
+namespace shabalin
+{
+  class Rectangle:
+    public Shape 
+  {
+    public:
+      Rectangle(point_t leftCorner, point_t rightCorner);
+      virtual double getArea() const;
+      virtual rectangle_t getFrameRect() const;
+      virtual void move(point_t);
+      virtual void move(double, double);
+      virtual void scaleShape(const double k);
+    private: 
+      point_t pointLeft;
+      point_t pointRight;
+  };
+}
+#endif
