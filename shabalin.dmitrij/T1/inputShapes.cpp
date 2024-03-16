3e8cb9696e2adb964dd608f434f663351cbf8f28
dmitei <dima9990860349@icloud.com>
2024-03-16 20:01:16 +0300

    this is omg

--- shabalin.dmitrij/T1/inputShapes.cpp
+++ shabalin.dmitrij/T1/inputShapes.cpp
@@ -1,18 +1,94 @@
 #include "inputShapes.hpp"
-#include "Rectangle.hpp"
 #include <string>
 #include <stdexcept>
 #include <stdexcept>
 
-void shabalin::creatingPoints(const size_t numberOfPoints, double *coordsOfArray, point_t *points)
+void shabalin::readPoints(std::istream &input, double *points, size_t count)
 {
-  for (size_t i = 0; i < numberOfPoints; ++i)
+  for (size_t i = 0; i < count; ++i)
   {
-    size_t index = i * 2;
-    points[i] = {coordsOfArray[index], coordsOfArray[index + 1]};
+    input >> points[i];
+  }
+   if (!input)
+  {
+    throw std::invalid_argument("Invalid parametrs");
   }
 }
 
+void shabalin::cleanOfShapes(Shape **Shapes, size_t &countOfShape)
+{
+  for (size_t i = 0; i < countOfShape; ++i)
+  {
+    delete Shapes[i];
+  }
+  delete[] Shapes;
+}
+
+void shabalin::readOfRectangle(std::istream &input, Shape **Shapes, size_t &countOfShape)
+{
+  double points[4]{};
+  shabalin::readPoints(input, points, 4);
+  Shapes[countOfShape] = new Rectangle({ points[0], points[1] } , { points[2], points[3] });
+}
+
+void shabalin::readOfSquare(std::istream &input, Shape **Shapes, size_t &countOfShape)
+{
+  double points[3]{};
+  shabalin::readPoints(input, points, 3);
+  Shapes[countOfShape] = new Square({ points[0], points[1] } , points[2]);
+}
+
+void shabalin::readOfTriangle(std::istream &input, Shape **Shapes, size_t &countOfShape)
+{
+  double points[6]{};
+  shabalin::readPoints(input, points, 6);
+  Shapes[countOfShape] = new Triangle({ points[0], points[1] } , { points[2], points[3] }, { points[4], points[5]});
+}
+
+void shabalin::readOfParallelogram(std::istream &input, Shape **Shapes, size_t &countOfShape)
+{
+  double points[6]{};
+  shabalin::readPoints(input, points, 6);
+  Shapes[countOfShape] = new Parallelogram({ points[0], points[1] } , { points[2], points[3] }, { points[4], points[5]});
+}
+
+void shabalin::readOfShapes(std::istream &input, Shape **Shapes, size_t &countOfShape, const std::string &name)
+{
+  try
+  {
+    if (name == "RECTANGLE")
+    {
+      readOfRectangle(input, Shapes, countOfShape);
+    }
+    else if (name == "TRIANGLE")
+    {
+      readOfTriangle(input, Shapes, countOfShape);
+    }
+    else if (name == "SQUARE")
+    {
+      readOfSquare(input, Shapes, countOfShape);
+    }
+    else if (name == "PARALLELOGRAM")
+    {
+      readOfParallelogram(input, Shapes, countOfShape);
+    }
+    else
+    {
+      throw std::runtime_error("Unknown shapes");
+    }
+    ++countOfShape;
+  }
+  catch (const std::bad_alloc& e)
+  {
+    cleanOfShapes(Shapes, countOfShape);
+    throw;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+}
+/*
 shabalin::Shape *shabalin::inputOfRectangle(const char str[])
 {
   const char *coordsOfString = str + 10;
@@ -32,3 +108,24 @@ shabalin::Shape *shabalin::inputOfRectangle(const char str[])
   creatingPoints(numberOfPoints, coordsOfArray, points);
   return new Rectangle(points[0], points[1]);
 }
+
+shabalin::Shape *shabalin::inputOfTriangle(const char str[])
+{
+  const char *coordsOfString = str + 9;
+  double coordsOfArray[6]{};
+  size_t position = 0;
+  for (size_t i = 0; i < 6; ++i)
+  {
+    coordsOfArray[i] = std::stod(coordsOfArray, std::addressof(position));
+    coordsOfString += position;
+  }
+  if (*coordsOfArray != '\0')
+  {
+    throw std::invalid_argument("Invalid arguments for Rectangle");
+  }
+  constexpr size_t numberOfPoints = 3;
+  point_t points[numberOfPoints]{};
+  creatingPoints(numberOfPoints, coordsOfArray, points);
+  return new Triangle(points[0], points[1], points[2]); 
+}
+*/
--- shabalin.dmitrij/T1/inputShapes.hpp
+++ shabalin.dmitrij/T1/inputShapes.hpp
@@ -1,12 +1,35 @@
 #ifndef INPUTSHAPES_HPP
 #define INPUTSHAPES_HPP
 #include "shape.hpp"
-#include <cstddef> 
+#include <cstddef>
+#include "rectangle.hpp"
+#include "Triangle.hpp"
+#include "square.hpp"
+#include "parallelogram.hpp"
 
 namespace shabalin
 {
+  /*
   void creatingPoints(const size_t numberOfPoints, double *coords, point_t *points);
-  Shape* inputOfRectangle(const char *);
+  Shape *inputOfRectangle(const char *);
+  //void shabalin::makeShapesClear(Shape **shapes, size_t count);
+  Shape *inputOfTriangle(const char str[])
+  void clearingShapes(Shape **shapes, size_t count);
+  void inputRectangle(std::istream &input, Shape **shapes, size_t &count);
+  void outputFrameRec(std::ostream &out, const Shape *shape);
+  void inputSquare(std::istream &input, Shape **shapes, size_t &count);
+  void inputDiamond(std::istream &input, Shape **shapes, size_t &count);
+  void inputParallelogram(std::istream &input, Shape **shapes, size_t &count);
+  Shape **inputShapes(std::istream &input, size_t &count);
+  void outputShapes(std::ostream &out, const Shape *const *shapes, size_t count);
+  */
+  void readPoints(std::istream &input, double *points, size_t count);
+  void readOfRectangle(std::istream &input, Shape **Shapes, size_t &countOfShape);
+  void readOfTriangle(std::istream &input, Shape **Shapes, size_t &countOfShape);
+  void readOfSquare(std::istream &input, Shape **Shapes, size_t &countOfShape);
+  void readOfParallelogram(std::istream &input, Shape **Shapes, size_t &countOfShape);
+  void readOfShapes(std::istream &input, Shape **Shapes, size_t &countOfShape, const std::string &name);
+  void cleanOfShapes(Shape **Shapes, size_t &countOfShape);
 }
 
 #endif
