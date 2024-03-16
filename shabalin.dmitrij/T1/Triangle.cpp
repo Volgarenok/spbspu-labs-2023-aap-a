781d0fabf6e66f6a47833ac6521e4df0324bbcb6
dmitei <dima9990860349@icloud.com>
2024-03-16 15:13:25 +0300

    heron theorem added

--- shabalin.dmitrij/T1/Triangle.cpp
+++ shabalin.dmitrij/T1/Triangle.cpp
@@ -1,15 +1,28 @@
 #include "Triangle.hpp"
 #include "shape.hpp"
 
-shabalin::triangle::triangle(point_t poi1, point_t poi2, point_t poi3):
-  point_ { p1, p2, p3 }
+shabalin::Triangle::Triangle(point_t p1, point_t p2, point_t p3):
+  p1_(p1),
+  p2_(p2),
+  p3_(p3)
 {
-  
+  double det = p1_.x * (p2_.y - p3_.y) - p2_.x * (p1_.y - p3_.y) + p3_.x * (p1_.y - p2_.y);
+  if (det == 0)
+  {
+    throw std::invalid_argument("Infinitely many solutions");
+  }
 }
 
-shabalin::triangle::~triangle() = default;
-
-double shabalin::triangle::getArea() const
+double shabalin::Triangle::getLength(point_t p1, point_t p2) const
 {
-  double 
+  return std::sqrt(std::pow(p2.x - p1.x, 2) + std::pow(p2.y - p1.y, 2));
+}
+
+double shabalin::Triangle::getArea() const
+{
+  double a = Triangle::getLength(p1_, p2_);
+  double b = Triangle::getLength(p2_, p3_);
+  double c = Triangle::getLength(p3_, p1_);
+  double perimetr = (a + b + c) / 2.0;
+  return std::sqrt(perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c))
 }
--- shabalin.dmitrij/T1/Triangle.hpp
+++ shabalin.dmitrij/T1/Triangle.hpp
@@ -4,18 +4,20 @@
 
 namespace shabalin
 {
-  class triangle: public Shape
+  class Triangle: public Shape
   {
     public: 
-      triangle(point_t, point_t, point_t);
+      Triangle(point_t, point_t, point_t);
       virtual double getArea() const;
       virtual rectangle_t getFrameRect() const;
       virtual void move(point_t);
       virtual void move(double, double);
       virtual void unsafeScale(const double ratio);
     private:
-      point_t point_[3]{};
-      point_t getCenter() const;
+      point_t p1_;
+      point_t p2_;
+      point_t p3_;
+      double getLength(point_t p1, point_t p2) const;
   };
 }
 #endif
