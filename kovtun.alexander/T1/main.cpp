70ade157f49d37a3d906e20a04d4c00e072bcdd7
puzz13 <puzz13one@gmail.com>
2024-03-17 16:52:44 +0300

    std::string draft input

--- kovtun.alexander/T1/main.cpp
+++ kovtun.alexander/T1/main.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
+#include <string>
+
+#include "base_types.h"
+#include "shape.h"
+#include "rectangle.h"
+
 int main()
 {
-  return 0;
+  std::string shapeName = "";
+  std::cin >> shapeName;
+
+  kovtun::Shape * shape = nullptr;
+  if (shapeName == "RECTANGLE")
+  {
+    kovtun::point_t rectanglePoints[2];
+    for (size_t i = 0; i < 2; i++)
+    {
+      std::cin >> rectanglePoints[i].x >> rectanglePoints[i].y;
+    }
+
+    if (!std::cin)
+    {
+      std::cerr << "bad input\n";
+      return 1;
+    }
+
+    shape = new kovtun::Rectangle();
+  }
 }
 
