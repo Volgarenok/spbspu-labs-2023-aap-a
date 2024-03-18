99ab9e4a7cd5b6eb5a8324cf9eba0f01a651708e
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-18 20:25:12 +0300

    I've changed my mind...

--- sakovskaia.maiia/T1/input.cpp
+++ sakovskaia.maiia/T1/input.cpp
@@ -1,11 +1,12 @@
 #include "input.hpp"
 #include <stddef>
 
-void sakovskaia::coordinates(const size_t cnt_points, double * coords_array, point_t * points)
+void sakovskaia::inputFigure(const char * string, double * coordinates, size_t cnt_points)
 {
+  size_t cnt_points = 0;
   for (size_t i = 0; i < cnt_points; ++i)
   {
-    size_t index = i * 2;
-    points[i] = {coords_array[index], coords_array[index + 1]};
+    coordinates[i] = std::stod(string, & cnt_points);
+    string += cnt_points;
   }
 }
--- sakovskaia.maiia/T1/input.hpp
+++ sakovskaia.maiia/T1/input.hpp
@@ -2,7 +2,7 @@
 #define INPUT_HPP
 namespace sakovskaia
 {
-  void coordinates(const size_t cnt_points, double * coords_array, point_t * points);
+  void coordinates(const char * string, double * coordinates, size_t cnt_points);
   Shape * inputRectangle(const char *);
 }
 #endif
