14dad85d97fd897fa66d0b06fbc482ad46ef9630
dmitei <dima9990860349@icloud.com>
2024-03-19 00:53:48 +0300

    cg

--- shabalin.dmitrij/T1/triangle.hpp
+++ shabalin.dmitrij/T1/triangle.hpp
@@ -6,9 +6,9 @@
 
 namespace shabalin
 {
-class Triangle : public Shape
-{
-public:
+  class Triangle : public Shape
+  {
+  public:
     Triangle(point_t p1, point_t p2, point_t p3);
     void move(point_t p) override;
     void move(double shift_x, double shift_y) override;
@@ -18,11 +18,11 @@ public:
     bool isTriangle(point_t p1, point_t p2, point_t p3);
     rectange_t getFrameRect() const override;
     Shape* clone() const override;
-private:
+  private:
     point_t p1_;
     point_t p2_;
     point_t p3_;
-};
+  };
 }
 
 #endif
