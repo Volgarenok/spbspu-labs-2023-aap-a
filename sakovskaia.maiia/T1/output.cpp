b9a30c54f8d0baab4c3795b882e082c8a8ca4147
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-18 22:22:09 +0300

    added output

---
+++ sakovskaia.maiia/T1/output.cpp
@@ -0,0 +1,22 @@
+#include "output.hpp"
+#include <ostream>
+
+void sakovskaia::outputShapes(std::ostream & output, const Shape * const shapes, size_t count)
+{
+  double sum = 0;
+  for (size_t i = 0; i < count; i++)
+  {
+    sum += shapes[i]->getArea();
+  }
+  output << sum;
+  for (size_t i = 0; i < count; i++)
+  {
+    rectangle_t rectangle = shapes[i]->getFrameRect();
+    double ll_x = rectangle.pos.x - rectangle.width / 2;
+    double ll_y = rectangle.pos.y - rectangle.height / 2;
+    double ur_x = rectangle.pos.x - rectangle.width / 2;
+    double ur_x = rectangle.pos.y - rectangle.height / 2;
+    output << " " << ll_x << " " << ll_y << " " << ur_x << " " << ur_y;
+  }
+  output << "\n";
+}
---
+++ sakovskaia.maiia/T1/output.hpp
@@ -0,0 +1,7 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+namespace sakovskaia
+{
+  void outputShapes(std::ostream & output, const Shape * const * shapes, size_t count);
+}
+#endif
