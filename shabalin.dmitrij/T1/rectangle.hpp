d5d570a8ce7fe5d857ee7c032929a8c1f44db3de
dmitei <dima9990860349@icloud.com>
2024-03-18 22:47:25 +0300

    add ma

--- shabalin.dmitrij/T1/rectangle.hpp
+++ shabalin.dmitrij/T1/rectangle.hpp
@@ -5,9 +5,9 @@
 
 namespace shabalin
 {
-  class Rectangle : public Shape
-  {
-  public:
+class Rectangle: public Shape
+{
+public:
     Rectangle(point_t bottom_left, point_t upper_right);
     virtual ~Rectangle() = default;
     virtual void move(point_t p) override;
@@ -15,11 +15,11 @@ namespace shabalin
     virtual void scale(double scale) override;
     virtual double getArea() const override;
     virtual rectange_t getFrameRect() const override;
-    virtual Shape *clone() const override;
-  private:
+    virtual Shape* clone() const override;
+private:
     Rectangle(point_t center, size_t width, size_t height);
     point_t center_;
     double width_, height_;
-  };
+};
 }
 #endif
