c532ed94543651f868252fa2d968a406e1caed1f
puzz13 <puzz13one@gmail.com>
2024-03-18 15:12:13 +0300

    Implement all methods (Ring)

--- kovtun.alexander/T1/ring.cpp
+++ kovtun.alexander/T1/ring.cpp
@@ -8,26 +8,30 @@ kovtun::Ring::Ring(const kovtun::point_t & center, double outerRadius, double in
 
 double kovtun::Ring::getArea() const
 {
-  return 0.0;
+  return (outerRadius_ * outerRadius_ - innerRadius_ * innerRadius_) * 3.141592;
 }
 
 kovtun::rectangle_t kovtun::Ring::getFrameRect() const
 {
-  return {};
+  double side = outerRadius_ * 2;
+  rectangle_t selfRect = { side, side, center_ };
+  return selfRect;
 }
 
 void kovtun::Ring::move(const kovtun::point_t & point)
 {
-
+  center_ = point;
 }
 
 void kovtun::Ring::move(double dx, double dy)
 {
-
+  center_.x += dx;
+  center_.y += dy;
 }
 
 void kovtun::Ring::scale(double k)
 {
-
+  outerRadius_ *= k;
+  innerRadius_ *= k;
 }
 
