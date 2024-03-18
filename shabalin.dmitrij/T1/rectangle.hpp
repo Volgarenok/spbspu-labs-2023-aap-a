a8d0749f22e814e912bd316879ecf5f747239c21
dmitei <dima9990860349@icloud.com>
2024-03-18 03:11:32 +0300

    ma bad toO

--- shabalin.dmitrij/T1/rectangle.hpp
+++ shabalin.dmitrij/T1/rectangle.hpp
@@ -1,21 +1,25 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
+
 #include "shape.hpp"
+
 namespace shabalin
 {
-  class Rectangle:
-    public Shape 
+  class Rectangle : public Shape
   {
-    public:
-      Rectangle(point_t leftCorner, point_t rightCorner);
-      virtual double getArea() const;
-      virtual rectangle_t getFrameRect() const;
-      virtual void move(const point_t);
-      virtual void move(double, double);
-      virtual void unsafeScale(const double ratio);
-    private: 
-      point_t pointLeft;
-      point_t pointRight;
+  public:
+    Rectangle(point_t bottom_left, point_t upper_right);
+    virtual ~Rectangle() = default;
+    virtual void move(point_t p) override;
+    virtual void move(double shift_x, double shift_y) override;
+    virtual void scale(double scale) override;
+    virtual double getArea() const override;
+    virtual rectange_t getFrameRect() const override;
+    virtual Shape *clone() const override;
+  private:
+    Rectangle(point_t center, size_t width, size_t height);
+    point_t center_;
+    double width_, height_;
   };
 }
 #endif
