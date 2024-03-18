8ed408455ba46f76503896b711c85821a3c6d13f
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-18 03:26:22 +0300

    added getting frame for rectangle

--- sakovskaia.maiia/T1/rectangle.cpp
+++ sakovskaia.maiia/T1/rectangle.cpp
@@ -1,4 +1,5 @@
 #include "rectangle.hpp"
+#include <cmath>
 
 sakovskaia::Rectangle::Rectangle(point_t ll_, point_t ur_):
   ll_(ll_),
@@ -10,4 +11,13 @@ double sakovskaia::Rectangle::getArea() const
   double height = ur_.y - ll_.y;
   double width = ur_.x - ll.x;
   return height * width;
+}
+
+sakovskaia::rectangle_t
+sakovskaia::Rectangle::getFrameRect() const
+{
+  double height = std::abs(ur_.y - ll_.y);
+  double width = std::abs(ur_.x - ll.x);
+  point_t center{((ur_.x + ll_.x) / 2), ((ur_.y + ll_.y) / 2)};
+  return {width, height, center};
 }
