8c63b923b0a3614e1f5e1a6fec74c224936bf0b7
puzz13 <puzz13one@gmail.com>
2024-03-18 14:18:56 +0300

    Add draft output

--- kovtun.alexander/T1/main.cpp
+++ kovtun.alexander/T1/main.cpp
@@ -18,9 +18,71 @@ int main()
     std::cin >> scaleCenter.x >> scaleCenter.y >> scaleRatio;
     if (scaleRatio <= 0.0)
     {
-      // TODO: throw
+      // TODO: throw?
     }
 
+    if (shapesCount == 0)
+    {
+      // TODO: throw?
+    }
+
+    double totalArea = 0.0;
+    for (size_t i = 0; i < shapesCount; i++)
+    {
+      if (!shapes[i])
+      {
+        continue;
+      }
+
+      totalArea += shapes[i]->getArea();
+    }
+    std::cout << totalArea << " ";
+    for (size_t i = 0; i < shapesCount; i++)
+    {
+      if (!shapes[i])
+      {
+        continue;
+      }
+
+      kovtun::rectangle_t selfRect = shapes[i]->getFrameRect();
+      kovtun::point_t leftBottomCorner = { selfRect.pos.x - selfRect.width / 2.0, selfRect.pos.y - selfRect.height / 2.0 };
+      kovtun::point_t rightTopCorner = { selfRect.pos.x + selfRect.width / 2.0, selfRect.pos.y + selfRect.height / 2.0 };
+      std::cout << leftBottomCorner.x << " " << leftBottomCorner.y << " " << rightTopCorner.x << " " << rightTopCorner.y << '\n';
+    }
+
+    for (size_t i = 0; i < shapesCount; i++)
+    {
+      if (!shapes[i])
+      {
+        continue;
+      }
+
+      kovtun::isotropicScale(shapes[i], scaleCenter, scaleRatio);
+    }
+
+    totalArea = 0.0;
+    for (size_t i = 0; i < shapesCount; i++)
+    {
+      if (!shapes[i])
+      {
+        continue;
+      }
+
+      totalArea += shapes[i]->getArea();
+    }
+    std::cout << totalArea << " ";
+    for (size_t i = 0; i < shapesCount; i++)
+    {
+      if (!shapes[i])
+      {
+        continue;
+      }
+
+      kovtun::rectangle_t selfRect = shapes[i]->getFrameRect();
+      kovtun::point_t leftBottomCorner = { selfRect.pos.x - selfRect.width / 2.0, selfRect.pos.y - selfRect.height / 2.0 };
+      kovtun::point_t rightTopCorner = { selfRect.pos.x + selfRect.width / 2.0, selfRect.pos.y + selfRect.height / 2.0 };
+      std::cout << leftBottomCorner.x << " " << leftBottomCorner.y << " " << rightTopCorner.x << " " << rightTopCorner.y << '\n';
+    }
 
   }
   catch (...)
