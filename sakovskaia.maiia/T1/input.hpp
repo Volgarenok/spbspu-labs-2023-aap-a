6b2c3f6261a9c7fe207a42d9f0581858ea2b659e
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 01:36:48 +0300

    fix

--- sakovskaia.maiia/T1/input.hpp
+++ sakovskaia.maiia/T1/input.hpp
@@ -1,6 +1,7 @@
 #ifndef INPUT_HPP
 #define INPUT_HPP
 #include <cstddef>
+#include "shape.hpp"
 namespace sakovskaia
 {
   void coorShapes(const char * string, double * coordinates, size_t cnt_points);
--- sakovskaia.maiia/T1/output.hpp
+++ sakovskaia.maiia/T1/output.hpp
@@ -1,6 +1,7 @@
 #ifndef OUTPUT_HPP
 #define OUTPUT_HPP
 #include <cstddef>
+#include "shape.hpp"
 namespace sakovskaia
 {
   void outputShapes(std::ostream & output, const Shape * const * shapes, size_t count);
