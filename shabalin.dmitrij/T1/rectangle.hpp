9faaf8aa3fc940a7af6821d78a32be54f044846a
dmitei <dima9990860349@icloud.com>
2024-03-19 00:18:58 +0300

    re

--- shabalin.dmitrij/T1/rectangle.hpp
+++ shabalin.dmitrij/T1/rectangle.hpp
@@ -5,22 +5,21 @@
 
 namespace shabalin
 {
-  class Rectangle : public Shape
-  {
-  public:
+class Rectangle: public Shape
+{
+public:
     Rectangle(point_t bottom_left, point_t upper_right);
-    virtual ~Rectangle() = default;
-    virtual void move(point_t p) override;
-    virtual void move(double shift_x, double shift_y) override;
-    virtual void scale(double scale) override;
-    virtual double getArea() const override;
-    virtual rectange_t getFrameRect() const override;
-    virtual Shape *clone() const override;
-
-  private:
+    ~Rectangle() = default;
+    void move(point_t p) override;
+    void move(double shift_x, double shift_y) override;
+    void scale(double scale) override;
+    double getArea() const override;
+    rectange_t getFrameRect() const override;
+    Shape* clone() const override;
+private:
     Rectangle(point_t center, size_t width, size_t height);
     point_t center_;
     double width_, height_;
-  };
+};
 }
 #endif
--- shabalin.dmitrij/T1/triangle.cpp
+++ shabalin.dmitrij/T1/triangle.cpp
@@ -1,6 +1,6 @@
 #include "triangle.hpp"
+
 #include <cmath>
-#include <algorithm>
 #include <stdexcept>
 
 namespace shabalin
@@ -18,28 +18,28 @@ p3_(p3)
 
 bool Triangle::isTriangle(point_t p1, point_t p2, point_t p3)
 {
-  const double d1 = distance(p1, p2);
-  const double d2 = distance(p1, p3);
-  const double d3 = distance(p2, p3);
-  return (d1 + d2 > d3) && (d1 + d3 > d2) && (d2 + d3 > d1);
+    const double d1 = distance(p1, p2);
+    const double d2 = distance(p1, p3);
+    const double d3 = distance(p2, p3);
+    return (d1 + d2 > d3) && (d1 + d3 > d2) && (d2 + d3 > d1);
 }
 
 double Triangle::distance(point_t p1, point_t p2)
 {
-  return std::sqrt(std::pow(p2.x - p1.x, 2.0) + std::pow(p2.y - p1.y, 2.0));
+    return std::sqrt(std::pow(p2.x - p1.x, 2.0) + std::pow(p2.y - p1.y, 2.0));
 }
 
 void Triangle::move(point_t p)
 {
-  double dx = p.x - (p1_.x + p2_.x + p3_.x) / 3.0;
-  double dy = p.y - (p1_.y + p2_.y + p3_.y) / 3.0;
-
-  p1_.x += dx;
-  p1_.y += dy;
-  p2_.x += dx;
-  p2_.y += dy;
-  p3_.x += dx;
-  p3_.y += dy;
+    double dx = p.x - (p1_.x + p2_.x + p3_.x) / 3.0;
+    double dy = p.y - (p1_.y + p2_.y + p3_.y) / 3.0;
+    
+    p1_.x += dx;
+    p1_.y += dy;
+    p2_.x += dx;
+    p2_.y += dy;
+    p3_.x += dx;
+    p3_.y += dy;
 }
 
 void Triangle::move(double shift_x, double shift_y)
@@ -56,7 +56,7 @@ void Triangle::scale(double scale)
 {
     double dx = p1_.x;
     double dy = p1_.y;
-
+    
     p1_.x = dx + (p1_.x - dx) * scale;
     p1_.y = dy + (p1_.y - dy) * scale;
     p2_.x = dx + (p2_.x - dx) * scale;
@@ -80,11 +80,11 @@ rectange_t Triangle::getFrameRect() const
     double max_x = std::max({p1_.x, p2_.x, p3_.x});
     double min_y = std::min({p1_.y, p2_.y, p3_.y});
     double max_y = std::max({p1_.y, p2_.y, p3_.y});
-
+    
     point_t center = {(min_x + max_x) / 2, (min_y + max_y) / 2};
     double width = max_x - min_x;
     double height = max_y - min_y;
-
+    
     return rectange_t{center, width, height};
 }
 
