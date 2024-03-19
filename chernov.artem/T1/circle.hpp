51d76e732508fd9612a2c150871a70010ef48f3f
CA21lmFlame <chernov.ar@spbstu.ru>
2024-03-19 11:27:23 +0300

    added circle

---
+++ chernov.artem/T1/circle.hpp
@@ -0,0 +1,25 @@
+#ifndef CIRCLE_HPP
+#define	CIRCLE_HPP
+#include "shape.hpp"
+#include "base-types.hpp"
+
+namespace chernov
+{
+  class Circle : public Shape
+  {
+  public:
+    Circle(point_t center, double radius);
+    ~Circle() = default;
+    double getArea() const override;
+    rectangle_t getFrameRect() override;
+    void move(point_t pos) override;
+    void move(double moveX, double moveY) override;
+    void scale(double multiplier) override;
+    Shape* clone() const override;
+  private:
+    point_t center_;
+    double radius_;
+  };
+}
+
+#endif
