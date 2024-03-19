f4c693c82cb5fee2472e8cae8e26b95dfce54089
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 04:02:54 +0300

    added gitFrameRect for square

--- sakovskaia.maiia/T1/square.cpp
+++ sakovskaia.maiia/T1/square.cpp
@@ -9,3 +9,10 @@ double sakovskaia::Square::getArea() const
 {
   return length_ * length_;
 }
+
+sakovskaia::rectangle_t
+sakovskaia::Square::getFrameRect() const
+{
+  point_t position = {ll_.x + (length_ / 2.0), ll_.y + (length_ / 2.0)};
+  return rectangle_t{length_, length_, position};
+}
