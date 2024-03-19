ead730df28d73ec3490caa36281a08dd557cb401
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 04:23:11 +0300

    added move for square

--- sakovskaia.maiia/T1/square.cpp
+++ sakovskaia.maiia/T1/square.cpp
@@ -13,6 +13,27 @@ double sakovskaia::Square::getArea() const
 sakovskaia::rectangle_t
 sakovskaia::Square::getFrameRect() const
 {
-  point_t position = {ll_.x + (length_ / 2.0), ll_.y + (length_ / 2.0)};
-  return rectangle_t{length_, length_, position};
+  point_t s_center = {ll_.x + (length_ / 2.0), ll_.y + (length_ / 2.0)};
+  return rectangle_t{length_, length_, s_center};
+}
+
+void sakovskaia::Square::move(const point_t new_center)
+{
+  point_t center = {ll_.x + (length_ / 2.0), ll_.y + (length_ / 2.0)};
+  double dx_ = new_center.x - center.x;
+  double dy_ = new_center.y - center.y;
+  this->move(dx_,  dy_);
+}
+
+void sakovskaia::Square::move(double dx, double dy)
+{
+  point_t center = {ll_.x + (length_ / 2.0), ll_.y + (length_ / 2.0)};
+  center.x += dx;
+  center.y += dy;
+  point_t * points[] = {&ll_, &ur_};
+  for (size_t i = 0; i < 2; i++)
+  {
+    points[i]->x += dx;
+    points[i]->y += dy;
+  }
 }
