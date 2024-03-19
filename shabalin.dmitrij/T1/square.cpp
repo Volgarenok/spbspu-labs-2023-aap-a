f049d5862a5e5e62894ca2d4e0ccb160dcef9f07
dmitei <dima9990860349@icloud.com>
2024-03-19 11:24:06 +0300

    idk

--- shabalin.dmitrij/T1/square.cpp
+++ shabalin.dmitrij/T1/square.cpp
@@ -1,63 +1,66 @@
 #include "square.hpp"
+
 #include <stdexcept>
 #include <algorithm>
 
 namespace shabalin
 {
-  Square::Square(double left_bottom_x, double left_bottom_y, double side_length) :
-  left_bottom_{left_bottom_x, left_bottom_y},
-  side_length_(side_length)
-  {
-     if (side_length <= 0.0)
+    Square::Square(double left_bottom_x, double left_bottom_y, double side_length) :
+        left_bottom_{left_bottom_x, left_bottom_y},
+        side_length_(side_length)
     {
-      throw std::invalid_argument("Side length of the square must be positive");
+        if (side_length <= 0)
+        {
+            throw std::invalid_argument("Side length of the square must be positive");
+        }
     }
-  }
-  Square::Square(point_t center, double side_length) :
-  left_bottom_{center.x - side_length / 2, center.y - side_length / 2},
-  side_length_(side_length)
-  {
-  }
-  
 
-  void Square::move(point_t p)
-  {
-    double dx = p.x - (left_bottom_.x + side_length_ / 2);
-    double dy = p.y - (left_bottom_.y + side_length_ / 2);
-    left_bottom_.x += dx;
-    left_bottom_.y += dy;
-  }
+    Square::Square(point_t center, double side_length) :
+        left_bottom_{center.x - side_length / 2, center.y - side_length / 2},
+        side_length_(side_length)
+    {
+        if (side_length <= 0)
+        {
+            throw std::invalid_argument("Side length of the square must be positive");
+        }
+    }
+    void Square::move(point_t p)
+    {
+        double dx = p.x - (left_bottom_.x + side_length_ / 2);
+        double dy = p.y - (left_bottom_.y + side_length_ / 2);
+        left_bottom_.x += dx;
+        left_bottom_.y += dy;
+    }
 
-  void Square::move(double shift_x, double shift_y)
-  {
-    left_bottom_.x += shift_x;
-    left_bottom_.y += shift_y;
-  }
+    void Square::move(double shift_x, double shift_y)
+    {
+        left_bottom_.x += shift_x;
+        left_bottom_.y += shift_y;
+    }
 
-  void Square::scale(double scale)
-  {
-    double center_x = left_bottom_.x + side_length_ / 2;
-    double center_y = left_bottom_.y + side_length_ / 2;
+    void Square::scale(double scale)
+    {
+        double center_x = left_bottom_.x + side_length_ / 2;
+        double center_y = left_bottom_.y + side_length_ / 2;
 
-    side_length_ *= scale;
+        side_length_ *= scale;
 
-    left_bottom_.x = center_x - side_length_ / 2;
-    left_bottom_.y = center_y - side_length_ / 2;
-  }
+        left_bottom_.x = center_x - side_length_ / 2;
+        left_bottom_.y = center_y - side_length_ / 2;
+    }
 
-  double Square::getArea() const
-  {
-    return side_length_ * side_length_;
-  }
+    double Square::getArea() const
+    {
+        return side_length_ * side_length_;
+    }
 
-  rectangle_t Square::getFrameRect() const
-  {
-    point_t pos = { left_bottom_.x + (side_length_ / 2.0), left_bottom_.y + (side_length_ / 2.0)};
-    return {pos, side_length_, side_length_};
-  }
+    rectangle_t Square::getFrameRect() const
+    {
+        return {left_bottom_, side_length_, side_length_};
+    }
 
-  Shape *Square::clone() const
-  {
-    return new Square(left_bottom_.x, left_bottom_.y, side_length_);
-  }
+    Shape* Square::clone() const
+    {
+        return new Square(left_bottom_.x, left_bottom_.y, side_length_);
+    }
 }
--- shabalin.dmitrij/T1/triangle.cpp
+++ shabalin.dmitrij/T1/triangle.cpp
@@ -10,15 +10,10 @@ p1_(p1),
 p2_(p2),
 p3_(p3)
 {
-  if (!isTriangle(p1, p2, p3))
-  {
-    throw std::invalid_argument("Can't create Triangle with given parameters");
-  }
-}
-
-double Triangle::distance(point_t p1, point_t p2)
-{
-    return std::sqrt(std::pow(p2.x - p1.x, 2.0) + std::pow(p2.y - p1.y, 2.0));
+    if (!isTriangle(p1, p2, p3))
+    {
+        throw std::invalid_argument("Can't create Triangle with given parameters");
+    }
 }
 
 bool Triangle::isTriangle(point_t p1, point_t p2, point_t p3)
@@ -29,11 +24,16 @@ bool Triangle::isTriangle(point_t p1, point_t p2, point_t p3)
     return (d1 + d2 > d3) && (d1 + d3 > d2) && (d2 + d3 > d1);
 }
 
+double Triangle::distance(point_t p1, point_t p2)
+{
+    return std::sqrt(std::pow(p2.x - p1.x, 2.0) + std::pow(p2.y - p1.y, 2.0));
+}
+
 void Triangle::move(point_t p)
 {
     double dx = p.x - (p1_.x + p2_.x + p3_.x) / 3.0;
     double dy = p.y - (p1_.y + p2_.y + p3_.y) / 3.0;
-
+    
     p1_.x += dx;
     p1_.y += dy;
     p2_.x += dx;
