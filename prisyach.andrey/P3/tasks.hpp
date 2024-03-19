096714eb3ffc1065d008b58007d2a50f3c6b8c38
BennyFit <andreyp.2005@gmail.com>
2024-03-19 17:39:51 +0300

    initial commit

---
+++ prisyach.andrey/P3/tasks.hpp
@@ -0,0 +1,15 @@
+#ifndef TASKS_HPP
+#define TASKS_HPP
+
+#include <cstddef>
+
+namespace prisyach
+{
+
+int isDuplicateCharacters(const char* str, size_t sizeOfString);
+
+char* mergeStrings(const char* str1, const char* str2, size_t sizeStr1);
+
+}
+
+#endif
