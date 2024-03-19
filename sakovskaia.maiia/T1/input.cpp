c36c3be0b60b106b6976166a00dc0ade695e3224
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 10:59:51 +0300

    added input for ellipse

--- sakovskaia.maiia/T1/input.cpp
+++ sakovskaia.maiia/T1/input.cpp
@@ -16,7 +16,7 @@ void sakovskaia::coorShape(const char * string, double * coordinates, size_t cnt
 sakovskaia::Shape * sakovskaia::inputRectangle(const char * string)
 {
   double rectangleCoordinates[4]{};
-  coorShapes(string, rectangleCoordinates, 4);
+  coorShape(string, rectangleCoordinates, 4);
   point_t ll = {rectangleCoordinates[0], rectangleCoordinates[1]};
   point_t ur = {rectangleCoordinates[2], rectangleCoordinates[3]};
   return new Rectangle(ll, ur);
@@ -25,7 +25,7 @@ sakovskaia::Shape * sakovskaia::inputRectangle(const char * string)
 sakovskaia::Shape * sakovskaia::inputParallelogram(const char * string)
 {
   double parallelogramCoordinates[6]{};
-  coorShapes(string, parallelogramCoordinates, 6);
+  coorShape(string, parallelogramCoordinates, 6);
   point_t p1 = {parallelogramCoordinates[0], parallelogramCoordinates[1]};
   point_t p2 = {parallelogramCoordinates[2], parallelogramCoordinates[3]};
   point_t p3 = {parallelogramCoordinates[4], parallelogramCoordinates[5]};
@@ -35,8 +35,16 @@ sakovskaia::Shape * sakovskaia::inputParallelogram(const char * string)
 sakovskaia::Shape * sakovskaia::inputSquare(const char * string)
 {
   double squareCoordinates[3]{};
-  coorShapes(string, parallelogramCoordinates, 3);
+  coorShape(string, parallelogramCoordinates, 3);
   point_t ll = {squareCoordinates[0], squareCoordinates[1]};
   double lehgth = squareCoordinates[2];
   return new Square(ll, length);
+}
+
+sakovskaia::Shape * sakovskaia::inputEllipse(const char * string)
+{
+  double ellipseCoordinates[4]{};
+  coorShape(string, ellipseCoordinates, 4);
+  point_t center = {ellipseCoordinates[0], ellipseCoordinates[1]};
+  return new Ellipse(center, ellipseCoordinates[2], ellipseCoordinates[3]);
 }
