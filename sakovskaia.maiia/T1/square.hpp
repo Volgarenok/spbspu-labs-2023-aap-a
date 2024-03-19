48bd7d2d89f590589865ab8c52a23e8e3d98bfdd
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 03:56:57 +0300

    added square.hpp

---
+++ sakovskaia.maiia/T1/square.hpp
@@ -0,0 +1,22 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+#include "shape.hpp"
+namespace sakovskaia
+{
+
+  class Square: public Shape
+  {
+  public:
+    Square(const point_t ll, double length);
+    double getArea() const;
+    rectangle_t getFrameRect() const;
+    void move(const point_t);
+    void move(double dx, double dy);
+    void unsafeScale(double k);
+
+  private:
+    point_t ll_;
+    double length_;
+  };
+}
+#endif
