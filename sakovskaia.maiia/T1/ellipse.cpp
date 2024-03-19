3fc973d23e0bc87344fd0cd35ba4570969203182
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-19 04:42:47 +0300

    added getFrameRect for ellipse

--- sakovskaia.maiia/T1/ellipse.cpp
+++ sakovskaia.maiia/T1/ellipse.cpp
@@ -11,3 +11,12 @@ double sakovskaia::Ellipse::getArea() const
   const double pi = 3.14;
   return pi * radiusX_ * radiusY_;
 }
+
+sakovskaia::rectangle_t
+sakovskaia::Ellipse::getFrameRect() const
+{
+  double height = radiusY_ * 2;
+  double width = radiusX_ * 2;
+  point_t center = center_;
+  return {width, height, center};
+}
